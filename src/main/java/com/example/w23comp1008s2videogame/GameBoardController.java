4ad380faead81c37b7501c76cbe4088a6d42f01e
JaretWright <jaret.wright@georgiancollege.ca>
2023-03-20 15:47:17 -0400

    Week 10 - Added the Sprite and Game config classes

--- src/main/java/com/example/w23comp1008s2videogame/GameBoardController.java
+++ src/main/java/com/example/w23comp1008s2videogame/GameBoardController.java
@@ -1,4 +1,32 @@
 package com.example.w23comp1008s2videogame;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+
 public class GameBoardController {
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private Button startButton;
+
+    @FXML
+    void startGame(ActionEvent event) {
+        startButton.setVisible(false);
+
+        //a canvas can be used to "draw" on.  The GraphicsContext is a tool used for
+        //the drawing
+        Canvas canvas = new Canvas(GameConfig.getGame_width(),GameConfig.getGame_height());
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        //get an Image object
+        Image shipImage = new Image(getClass().getResourceAsStream("images/ship.png"));
+    }
+
 }
