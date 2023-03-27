d571e69d16658a11f0e0b132d0c48df95170243b
JaretWright <jaret.wright@georgiancollege.ca>
2023-03-27 13:55:09 -0400

    Week 11 - Added code to capture which keys are being pressed on the keyboard.

--- src/main/java/com/example/w23comp1008s2videogame/GameBoardController.java
+++ src/main/java/com/example/w23comp1008s2videogame/GameBoardController.java
@@ -7,7 +7,10 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
+
+import java.util.HashSet;
 
 public class GameBoardController {
 
@@ -17,9 +20,25 @@ public class GameBoardController {
     @FXML
     private Button startButton;
 
+    private HashSet<KeyCode> activeKeys;
+
     @FXML
     void startGame(ActionEvent event) {
         startButton.setVisible(false);
+
+        //create a HashSet to store the keys that are currently pressed
+        //A HashSet is like an ArrayList except that it prevents duplicates
+        //and it does not preserve the order of elements.  This is not a specific piece of knowledge
+        //you need for this course, it is covered in the datastructure side of Advanced Java.  However,
+        //employers have been known to ask about it during job interviews, so I'm giving you a sneak peak!
+        activeKeys = new HashSet<>();
+
+        //register when a key is pressed or released
+        //When we use -> {} it is a "lambda expression".  Essentially, it is a shortcut way of creating an
+        //anonymous inner class and calling a method
+        anchorPane.getScene().setOnKeyPressed(keyPressed -> activeKeys.add(keyPressed.getCode()));
+        anchorPane.getScene().setOnKeyReleased(keyReleased -> activeKeys.remove(keyReleased.getCode()));
+
         //a canvas can be used to "draw" on.  The GraphicsContext is a tool used for
         //the drawing
         Canvas canvas = new Canvas(GameConfig.getGame_width(),GameConfig.getGame_height());
@@ -29,7 +48,7 @@ public class GameBoardController {
         Image shipImage = new Image(getClass().getResourceAsStream("images/ship.png"));
         Image background = new Image(getClass().getResourceAsStream("images/space.png"));
 
-        Sprite ship = new Sprite(shipImage,400, 100,100,70,10);
+        Sprite ship = new Sprite(shipImage,100, 100,100,70,1);
 
         AnimationTimer timer = new AnimationTimer() {
             @Override
@@ -45,3 +64,17 @@ public class GameBoardController {
     }
 
 }
+
+
+/**
+ *
+ * ArrayList -> [A,A,A,A,A,A,A,A]
+ * Stack -> push items onto the stack and pop them off.
+ * Set -> Sets are much like list - they are a collection, however, they automatically prevent
+ *              duplicates
+ * [A]
+ *
+ *
+ *
+ *
+ */
